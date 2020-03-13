c9bdfe6a83f9215738f726f63527a1f68a779be6
Gabrielius <gabrielius914@gmail.com>
2020-03-13 20:20:50 +0200

    BackendRouter.js

--- assets/js/Components/backendRouter.js
+++ assets/js/Components/backendRouter.js
@@ -1,4 +1,5 @@
-const router = require('../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min');
+const router = require('../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min');
+
 // dumped_routes.json is the output file for the fos:js-routing:dump command
 const routerConfig = require('../../../public/js/fos_js_routes.json');
 
